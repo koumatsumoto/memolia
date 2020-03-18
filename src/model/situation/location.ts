7d041f0503c36a61fac4bfda8eb4f3597d9458b5
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-03-18 12:10:51 +0900

    feat(model): add situation

---
+++ src/model/situation/location.ts
@@ -0,0 +1,23 @@
+import { Brand } from '../../common/type';
+
+export type Area = Brand<string, 'Area'>;
+
+/**
+ * TODO: not implemented
+ *
+ * Determine area name such as "tokyo", "shibuya" by coordinates.
+ *
+ * @param coordinates
+ */
+export const getArea = (coordinates: Coordinates): Area => {
+  return '' as Area;
+}
+
+export class Location {
+  readonly area: Area;
+  private readonly coordinates: Coordinates;
+
+  constructor(coordinates: Coordinates) {
+    this.area = getArea(coordinates);
+  }
+}

d303bfc0c61b502d75fbc17c106b3e3cae60913f
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-03-21 18:02:16 +0900

    feat(app): add new page to view term

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AddTermComponent } from './page/add-term/add-term.component';
+import { ViewTermsComponent } from './page/view-terms/view-terms.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AppComponent,
+  },
+  {
+    path: 'terms',
+    component: ViewTermsComponent,
   },
   {
     path: 'terms/add',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AddTermComponent } from './page/add-term/add-term.component';
+import { ViewTermsComponent } from './page/view-terms/view-terms.component';
 
 @NgModule({
-  declarations: [AppComponent, AddTermComponent],
+  declarations: [AppComponent, AddTermComponent, ViewTermsComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],

ee00f94a5660ed219104146f63f7285fff6d64ba
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-03-22 14:43:18 +0900

    chore: integrate jest (#15)

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  rootDir: '.',
+  testURL: 'http://localhost/',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '<rootDir>/src/test.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/specs/',
+    '/test-helpers/',
+    '/testing/',
+    '/tests/',
+    '/dist/',
+  ],
+  coverageReporters: ['lcov', 'text-summary'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  verbose: true,
+  restoreMocks: true,
+  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+};

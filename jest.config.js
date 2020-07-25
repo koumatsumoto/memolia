1ff492a3a0b278394eb9224779e49597167eab8f
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-07-25 20:25:19 +0900

    build: update prettier (#24)

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,7 @@ module.exports = {
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '<rootDir>/src/test.ts'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/specs/',
-    '/test-helpers/',
-    '/testing/',
-    '/tests/',
-    '/dist/',
-  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/specs/', '/test-helpers/', '/testing/', '/tests/', '/dist/'],
   coverageReporters: ['lcov', 'text-summary'],
   globals: {
     'ts-jest': {
